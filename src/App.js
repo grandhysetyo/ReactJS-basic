bf0c2847b88a24a3b0c074506b573fa2826c2b01
grandhysetyo <setyo.grandhys1@gmail.com>
2021-04-08 10:44:53 +0700

    study component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import Navbar from './component/Navbar.js'
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h2>Hello, World!!</h2>
     </div>
   );
---
+++ src/component/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// example component arrow function 
+// => see documetation extension ES7 React Visual Code 
+const Navbar = () => {
+  return (
+    <div>
+        <h2>Navbar</h2>
+    </div>
+  )
+}
+
+export default Navbar
